3323488c653d242e4a9d3c564e4c2f86e676aba1
Kendall Comey <kendall.comey@gmail.com>
2015-05-05 13:24:35 -0700

    http assignment

--- httpServer/src/requestHandlers.js
+++ httpServer/src/requestHandlers.js
@@ -1,52 +1,43 @@
 var querystring = require("querystring");
 var url = require("url");
-  fs = require("fs");
+fs = require("fs");
 
 function time(response) {
   console.log("Request handler to 'time' was called.");
-  var serverTime = formatDate();
+  var serverTime =  new Date();
 
-  var body = '<html>'+
-    '<head>'+
-    '<meta http-equiv="Content-Type" '+
-    'content="text/html; charset=UTF-8" />'+
-    '</head>'+
-    '<body>'+
-    '<h3>The current time is '+ serverTime + '</h3>'+
-    '</body>'+
-    '</html>';
+  var body = '<html>' +
+  '<head>' +
+  '<meta http-equiv="Content-Type" ' +
+  'content="text/html; charset=UTF-8" />' +
+  '</head>' +
+  '<body><h3>Today\'s date/time is ' +
+  serverTime +
+  '</h3></body>' +
+  '</html>';
 
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(body);
-    response.end();
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(body);
+  response.end();
 }
 
 function greet(response, request, name) {
   console.log("Request handler 'greet' was called.");
   if (!name) { name = "Stranger";}
 
-  var body = '<html>'+
-    '<head>'+
-    '<meta http-equiv="Content-Type" '+
-    'content="text/html; charset=UTF-8" />'+
-    '</head>'+
-    '<body>'+
-    '<h3>Howdy '  + name + '!</h3>'+
-    '</body>'+
+  var body = '<html>' +
+    '<head>' +
+    '<meta http-equiv="Content-Type" ' +
+    'content="text/html; charset=UTF-8" />' +
+    '</head>' +
+    '<body>' +
+    '<h3>Howdy '  + name + '!</h3>' +
+    '</body>' +
     '</html>';
 
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(body);
-    response.end();
-}
-
-function formatDate() {
-  var d = new Date();
-  var hours = d.getHours();
-  var minutes = d.getMinutes();
-  var seconds = d.getSeconds();
-  var serverTime = hours + ':' + minutes + ':' + seconds;
-  return serverTime;
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(body);
+  response.end();
 }
 
 exports.time = time;
--- httpServer/test/test.js
+++ httpServer/test/test.js
@@ -5,20 +5,59 @@ var expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('http server', function() {
-  it('expect status to be code 200', function (done) {
+  it('expect status code to be 200', function (done) {
     chai.request('http://localhost:8888').get('/').
-    end(function(err, response) {
-      expect(response).to.have.status(200);
+    end(function(err, res) {
+      expect(res).to.have.status(200);
       done();
     }, function (err) {
       throw err;
     });
   });
 
-  it('expect greet/Kendall to return same', function(done) {
+  it('expect content to be html', function (done) {
+    chai.request('http://localhost:8888').get('/').
+    end(function(err, res) {
+      expect(res).to.be.html;
+      done();
+    }, function (err) {
+      throw err;
+    });
+  });
+
+  // Testing /greet
+  it('expect /greet/Kendall to return Howdy Kendall!', function(done) {
     chai.request('http://localhost:8888').get('/greet/Kendall').
-    end(function(err, response) {
-      expect(response.text.indexOf('Howdy Kendall!') > 0);
+    end(function(err, res) {
+      expect(res.text).to.contain('Howdy Kendall!');
+      done();
+    }, function (err) {
+      throw err;
+    });
+  });
+
+  // Testing /time
+  it('expect /time to return current time', function(done) {
+    chai.request('http://localhost:8888').get('/time').
+    end(function(err, res) {
+      // Time now
+      var nowDate = new Date();
+      // Time returned from page request
+      var serverDate = res.text.match(/is\s(.+)<\/h3>/i);
+      serverDate = Date.parse(serverDate[1]);
+      nowDate = Date.parse(nowDate);
+      expect(serverDate).to.be.closeTo(nowDate, 1);
+
+      done();
+    }, function (err) {
+      throw err;
+    });
+  });
+
+  it('expect content to have header of content-type: text/html', function (done) {
+    chai.request('http://localhost:8888').get('/').
+    end(function(err, res) {
+      expect(res).to.have.header('content-type', 'text/html');
       done();
     }, function (err) {
       throw err;
