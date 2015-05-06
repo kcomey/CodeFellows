09dcb822646fd22c93f75ae4a8eb4cf58bf46c8e
Kendall Comey <kendall.comey@gmail.com>
2015-05-06 14:59:21 -0700

    http2 assignment

---
+++ httpserver2/Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+  jshint: {
+    all: {
+      src: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        node: true,
+        mocha: true,
+        expr: true
+      }
+    }
+  },
+
+  simplemocha: {
+     src: ['test/**/*.js']
+  },
+
+jscs: {
+    src: '',
+    options: {
+        config: ".jscsrc"
+    }
+},
+
+  watch: {
+    files: ['test/**/*.js',''],
+    tasks: ['jshint', 'simplemocha', 'jscs']
+  }
+});
+
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-simple-mocha');
+grunt.loadNpmTasks('grunt-jscs');
+
+grunt.registerTask('default', ['jshint', 'simplemocha', 'jscs']);
+grunt.registerTask('test', ['simplemocha']);
+
+};
