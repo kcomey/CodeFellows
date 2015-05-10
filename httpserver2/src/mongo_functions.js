aa8b45edfb75628194b4725ca7eed629696bb2c1
Kendall Comey <kendall.comey@gmail.com>
2015-05-10 10:53:47 -0700

    http2 assignment

--- httpserver2/src/mongo_functions.js
+++ httpserver2/src/mongo_functions.js
@@ -1,6 +1,42 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/classroom-examples';
 
+var deleteDocument = function(req, res) {
+  var noteNum = Number(req.params.num);
+  // Set up the connection to the local db
+  MongoClient.connect(url, function(err, db) {
+    if (err) {
+      return console.dir(err);
+    }
+    // Connect to the collection
+    db.collection('notes', {strict: true}, function(err, collection) {
+      if (err) {
+        return console.dir(err);
+      }
+      // Ready to rock and roll
+      var query = { noteId: noteNum };
+
+      collection.remove({ noteId: 2 });
+
+      //console.log(typeof result);
+      //console.log(result);
+      console.log('worked, check db');
+      // returns the number of documents removed
+      console.log(result.nRemoved);
+
+        console.log('works here?');
+        if (!result) {
+          res.send('Note not found. Try another number.');
+        } else {
+        var body = 'Your note (note id: ' + req.params.num +
+          ') has been deleted.';
+        res.send(body);
+        }
+
+    });
+  });
+};
+
 var insertDocument = function(json) {
   // Set up the connection to the local db
   MongoClient.connect(url, function(err, db) {
@@ -41,12 +77,16 @@ var getDocument = function(req, res) {
       var query = { noteId: noteNum};
       collection.find(query).toArray(function(err, documents) {
         if (err) {
-          return console.dir(err);
+          throw err;
         }
       var note = documents[0];
-      var body = 'Here is the note you requested: <br>' +
-        note.noteBody + '<br>' + '(note id: ' + note.noteId + ')';
-        res.send(body);
+      if (!note) {
+        res.send('Note not found. Try another number.');
+      } else {
+        var body = 'Here is the note you requested: <br>' +
+          note.noteBody + '<br>' + '(note id: ' + note.noteId + ')';
+          res.send(body);
+      }
       });
     });
   });
@@ -54,6 +94,7 @@ var getDocument = function(req, res) {
 
 exports.getDocument = getDocument;
 exports.insertDocument = insertDocument;
+exports.deleteDocument = deleteDocument;
 
 
 
